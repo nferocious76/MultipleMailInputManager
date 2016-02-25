228cbdb6e5919bd0c2160e1c833cb16b68b2e68a
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2016-02-25 21:17:33 +0800

    Initial Commit

---
+++ MultipleMailInputManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultipleMailInputManager
+//
+//  Created by Neil Francis Hipona on 25/02/2016.
+//  Copyright © 2016 NFerocious. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
